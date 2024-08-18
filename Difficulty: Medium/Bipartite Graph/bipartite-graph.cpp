ba9aba4b72f0e8f89bcbb5e9dd3ecb8b1b964451
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2024-08-18 11:48:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
@@ -5,40 +5,35 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-
-
-   bool checkBipartiteDFS(vector<int>adj[],int curr, vector<int>&color,int currColor){
-       color[curr]=currColor;
-       
-       // iske neighbour ko color krte hai
-       for (auto v:adj[curr]){
-           if (color[v]==color[curr])return false;
-           if (color[v]==-1){
-               int currColorOfV=1-currColor;//yaad krloh
-               if (checkBipartiteDFS(adj,v,color,currColorOfV)==false) return false;
-           }
-       }
-       return true;
-       
-   }
-
-
-
+bool bfs(int u,vector<int>adj[], vector<int>&color){
+      queue<int>q;
+	   q.push(u);
+	   color[u]=0;
+	   while(!q.empty()){
+	       int node=q.front();
+	       q.pop();
+	       //neghbour
+	       for(auto v:adj[node]){
+	           if (color[v]==-1){
+	               color[v]=1-color[node];
+	               q.push(v);
+	           }else if (color[v]==color[node])return false;
+	       }
+	       
+	       
+	   }
+	   return true;
+}
 	bool isBipartite(int V, vector<int>adj[]){
-	   
+	  
 	   vector<int>color(V,-1);
-	   // 1->red
-	   //0->green
 	   for (int i=0;i<V;i++){
 	       if (color[i]==-1){
-	           if (checkBipartiteDFS(adj,i,color,1)==false) return false;
+	           if (bfs(i,adj,color)==false)return false;
 	       }
 	   }
-	   
 	   return true;
-	   
-	   
-	   
+	 
 	}
 
 };
